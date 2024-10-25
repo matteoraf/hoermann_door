1d312d3f78c52ecad19b10aedea492aeb667c0c3
Yannick Weber <14yannick@gmail.com>
2024-10-25 20:33:14 +0200

    Correct light Component adapt YAML

--- esphome/components/uapbridge_pic16/light/uapbridge_pic16_light.cpp
+++ esphome/components/uapbridge_pic16/light/uapbridge_pic16_light.cpp
@@ -4,7 +4,7 @@
 namespace esphome {
 namespace uapbridge_pic16 {
 
-static const char *TAG = "uapbridge_pic16.light";
+static const char* TAG = "uapbridge_pic16.light";
 
 light::LightTraits UAPBridge_pic16Light::get_traits() {
   auto traits = light::LightTraits();
@@ -13,14 +13,13 @@ light::LightTraits UAPBridge_pic16Light::get_traits() {
 }
 
 void UAPBridge_pic16Light::setup() {
-    ESP_LOGD(TAG, "UAPBridge_pic16Light::setup() - setup method called");
-    this->parent_->add_on_state_callback([this]() { this->on_event_triggered(); });
+  ESP_LOGD(TAG, "UAPBridge_pic16Light::setup() - setup method called");
+  this->parent_->add_on_state_callback([this]() { this->on_event_triggered(); });
 }
 
-void UAPBridge_pic16Light::write_state(light::LightState *state) {
+void UAPBridge_pic16Light::write_state(light::LightState* state) {
   bool binary;
   state->current_values_as_binary(&binary);
-  
   if (binary) {
     this->output_->turn_on();
   } else {
@@ -29,7 +28,7 @@ void UAPBridge_pic16Light::write_state(light::LightState *state) {
 }
 
 void UAPBridge_pic16Light::on_event_triggered() {
-  if (this->parent_->get_pic16_com() && this->state_->current_values.is_on() != this->parent_->get_light_enabled()) {
+  if (this->state_->current_values.is_on() != this->parent_->get_light_enabled()) {
     // Adjust the state of the light based on the external light state
     ESP_LOGD(TAG, "UAPBridge_pic16Light::update() - adjusting state");
     if (this->parent_->get_light_enabled()) {
